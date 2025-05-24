75794956994a9a99762ddc4b8162c655085abd10
Diego <144078438+diego4500@users.noreply.github.com>
2025-05-24 15:37:57 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -22,4 +22,4 @@ app.get('/text', (_, res) =>
 );
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Servidor na porta ${PORT}`));
+app.listen(PORT, () => console.log(` Servidor na porta ${PORT}`));
